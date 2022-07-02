46e124252d7fcacb17dd58935ec7d9df955eeb8c
uuyeong <proyeongs@gmail.com>
2022-07-02 14:56:25 +0900

    update 방금그곡

--- programmers/find_music.js
+++ programmers/find_music.js
@@ -1,8 +1,5 @@
-// 방금 그곡
+// 방금 그곡 복습완료
 //6.30;
-
-//꼭 다시풀기
-//
 
 // 문자열을 쪼개서 풀었는데 더 좋은 풀이를 찾아봐야할 것 같다.
 function solution(m, musicinfos) {
@@ -35,6 +32,47 @@ function solution(m, musicinfos) {
       realMusic += names;
     }
     realMusic = realMusic + names.slice(0, time % names.length);
+
+    if (realMusic.includes(m)) {
+      if (longTake < time) {
+        longTake = time;
+        answer = music[2];
+      }
+    }
+  });
+
+  return answer;
+}
+
+// 풀이 2번
+// replace시 정규식을 활용한다. 그런데 E#의경우는 바뀔경우 특정 케이스에서 오류가 발생해서 다시 원복해준다.
+// date로 별도로 split 하는것 대신 해당 시간으로 Date 객체를 만들어 시간차를 구한다.
+function solution(m, musicinfos) {
+  var answer = "(None)";
+
+  m = m.replace(/\w#/g, (a) => a[0].toLowerCase());
+  m = m.replace(/e/g, "E#");
+
+  let longTake = 0;
+
+  const musics = musicinfos.map((music) => {
+    return music.split(",");
+  });
+  musics.forEach((music) => {
+    let names = music[3].replace(/[A-Z]#/g, (music) => music[0].toLowerCase());
+    names = names.replace(/e/g, "E#");
+
+    // 구한 시간차는 ms단위 이므로 /60000으로 다시 분처리가 필요하다.
+    const time =
+      (new Date(`1970-01-01 ${music[1]}:00`) -
+        new Date(`1970-01-01 ${music[0]}:00`)) /
+      60000;
+
+    let realMusic = "";
+
+    realMusic =
+      names.repeat(Math.floor(time / names.length)) +
+      names.slice(0, time % names.length);
 
     if (realMusic.includes(m)) {
       if (longTake < time) {
