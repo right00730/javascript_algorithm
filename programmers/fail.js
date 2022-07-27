13549e11770b24fbf66e5a75052946934a185903
uuyeong <proyeongs@gmail.com>
2022-07-27 10:41:57 +0900

    실패율 케이스 추가

--- programmers/fail.js
+++ programmers/fail.js
@@ -29,3 +29,25 @@ function solution(N, stages) {
 
   return answer;
 }
+
+//case 2
+// stage를 구분할때 filter 사용 > O(n^2)
+//코드는 더 깔끔하지면 2중루프로 성능하락
+function solution(N, stages) {
+  var answer = [];
+  //fail = 스테이지에 도달하였으나 클리어하지못한 유저 / 총 도달 유저
+
+  //총 유저,
+  let totalUser = stages.length;
+  let fails = [];
+
+  for (let i = 1; i <= N; i++) {
+    let remained = stages.filter((stage) => stage === i).length;
+    fails.push([i, remained / totalUser]);
+    totalUser -= remained;
+  }
+
+  answer = fails.sort((a, b) => b[1] - a[1]).map((fail) => fail[0]);
+
+  return answer;
+}
