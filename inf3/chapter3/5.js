6003a92e3796d7a919372744d625f4afa8a51f0b
uuyeong <proyeongs@gmail.com>
2024-02-16 23:58:08 +0900

    Chapter3 /  문자열 압축  / Easy

---
+++ inf3/chapter3/5.js
@@ -0,0 +1,19 @@
+function solution(str) {
+  let answer = [str[0]];
+  let counter = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      counter++;
+      continue;
+    }
+    if (str[i] !== str[i - 1]) {
+      answer.push(counter);
+      answer.push(str[i]);
+      counter = 1;
+    }
+  }
+  return answer;
+}
+let str = "KKHSSSSSSSE";
+console.log(solution(str));
